17713904d30e625b92e1312b2b9298abda4708ac
gabrieldev-1220 <gabrieldev.1220@gmail.com>
2025-11-02 20:15:29 -0300

    Corregido Prescripciones_Resetas y Facturación

--- src/app/modules/pacientes/pacientes-routing.module.ts
+++ src/app/modules/pacientes/pacientes-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PacientesListComponent } from './pacientes-list/pacientes-list.component';
 import { PacientesFormComponent } from './pacientes-form/pacientes-form.component';
-import { PacienteDetailComponent } from './paciente-detail/paciente-detail.component';
+import { PacienteFichaComponent } from './paciente-ficha/paciente-ficha.component';
 
 const routes: Routes = [
   { path: '', component: PacientesListComponent },
   { path: 'new', component: PacientesFormComponent },
   { path: ':id/edit', component: PacientesFormComponent },
-  { path: ':id', component: PacienteDetailComponent }
+  { path: ':id', component: PacienteFichaComponent }  // ← AHORA USA FICHA MODERNA
 ];
 
 @NgModule({
